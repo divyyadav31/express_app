1d2f09bb819cb7a36ae387212598777de5731631
divyyadav31 <divyyadav31@jumbaya.com>
2024-06-15 23:44:03 +0530

    diary

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,37 @@
 import express from "express";
 import bodyParser from "body-parser";
-import fs from 'fs';
+import fs from "fs";
 import path from "path";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const configFilePath = path.resolve(__dirname, 'config.json');
+const configFilePath = path.resolve(__dirname, "config.json");
 
 app.use(bodyParser.json());
 
-if(! fs.existsSync(configFilePath)) {
-    fs.writeFileSync(configFilePath, JSON.stringify({}));
+if (!fs.existsSync(configFilePath)) {
+  fs.writeFileSync(configFilePath, JSON.stringify({}));
 }
 
-app.get('/api/config', (req,res) => {
-    fs.readFile(configFilePath, 'utf8', (err, data) => {
-        if(err) return res.status(500).json({ error: 'Failed to read config file' });
-        res.json(JSON.parse(data));
-    });
+app.get("/api/config", (req, res) => {
+  fs.readFile(configFilePath, "utf8", (err, data) => {
+    if (err)
+      return res.status(500).json({ error: "Failed to read config file" });
+    res.json(JSON.parse(data));
+  });
 });
 
-app.put('/api/config', (req, res) => {
-    fs.writeFile(configFilePath, JSON.stringify(req.body, null, 2), (err) => {
-        if(err) return res.status(500).json({ error: 'Failed to write config file' });
-        res.status(200).json({ message: 'Config updated successfully' });
-    });
+app.put("/api/config", (req, res) => {
+  fs.writeFile(configFilePath, JSON.stringify(req.body, null, 2), (err) => {
+    if (err)
+      return res.status(500).json({ error: "Failed to write config file" });
+    res.status(200).json({ message: "Config updated successfully" });
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
