5115fb3ab119ff273fe6b3836fde09e760646fb5
divyyadav31 <divyyadav31@jumbaya.com>
2024-06-19 16:41:49 +0530

    diary

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import fs from "fs";
 import path from "path";
 import * as dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const port = process.env.PORT || 5000;
 const configFilePath = path.resolve(__dirname, "config.json");
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 if (!fs.existsSync(configFilePath)) {
   fs.writeFileSync(configFilePath, JSON.stringify({}));
@@ -25,10 +28,12 @@ app.get("/api/config", (req, res) => {
 });
 
 app.put("/api/config", (req, res) => {
-  fs.writeFile(configFilePath, JSON.stringify(req.body, null, 2), (err) => {
-    if (err)
+  const jsonObject = JSON.parse(req.body.configData);
+  fs.writeFile(configFilePath, JSON.stringify(jsonObject, null, 2), (err) => {
+    if (err) {
       return res.status(500).json({ error: "Failed to write config file" });
-    res.status(200).json({ message: "Config updated successfully" });
+    }
+    res.status(200).json({ message: "Data successfully Updated" });
   });
 });
 
